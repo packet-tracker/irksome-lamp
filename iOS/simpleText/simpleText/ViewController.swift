3db7e89f83f5ac492440aa7f8bb7ab92f8fb1eb1
nooyahs <nooyahs@nooyahs.local>
2015-10-28 23:23:30 +0900

    organize folder

---
+++ iOS/simpleText/simpleText/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  simpleText
+//
+//  Created by nooyahs on 2015. 10. 28..
+//  Copyright © 2015년 nooyahs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
